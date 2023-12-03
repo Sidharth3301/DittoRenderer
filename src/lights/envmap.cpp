ff2021adbc942327892e0371fdfe0789d6e7e668
Sidharth Nair <iamsidharthnair@gmail.com>
2023-12-03 19:22:58 +0100

    IBL updated

--- src/lights/envmap.cpp
+++ src/lights/envmap.cpp
@@ -1,64 +1,76 @@
 #include <lightwave.hpp>
 
-namespace lightwave {
+namespace lightwave
+{
 
-class EnvironmentMap final : public BackgroundLight {
-    /// @brief The texture to use as background
-    ref<Texture> m_texture;
+    class EnvironmentMap final : public BackgroundLight
+    {
+        /// @brief The texture to use as background
+        ref<Texture> m_texture;
 
-    /// @brief An optional transform from local-to-world space
-    ref<Transform> m_transform;
+        /// @brief An optional transform from local-to-world space
+        ref<Transform> m_transform;
 
-public:
-    EnvironmentMap(const Properties &properties) {
-        m_texture   = properties.getChild<Texture>();
-        m_transform = properties.getOptionalChild<Transform>();
-    }
-
-    BackgroundLightEval evaluate(const Vector &direction) const override {
-        Vector2 warped = Vector2(0, 0);
-        // hints:
-        // * if (m_transform) { transform direction vector from world to local
-        // coordinates }
-        // * find the corresponding pixel coordinate for the given local
-        // direction
-        auto local_dir = m_transform->inverse(direction);
-        if (m_transform)
+    public:
+        EnvironmentMap(const Properties &properties)
         {
-            float phi = atan2f(local_dir.z(),local_dir.x())-Pi;
-            float theta = acosf(local_dir.y()/local_dir.length());
-            warped.x() = theta/Pi;
-            warped.y() = (phi+Pi)/(2*Pi);
+            m_texture = properties.getChild<Texture>();
+            m_transform = properties.getOptionalChild<Transform>();
         }
-        return {
-            .value = m_texture->evaluate(warped),
-        };
-    }
 
-    DirectLightSample sampleDirect(const Point &origin,
-                                   Sampler &rng) const override {
-        Vector direction = squareToUniformSphere(rng.next2D());
-        auto E           = evaluate(direction);
+        BackgroundLightEval evaluate(const Vector &direction) const override
+        {
+            Vector2 warped = Vector2(0, 0);
+            // hints:
+            // * if (m_transform) { transform direction vector from world to local
+            // coordinates }
+            // * find the corresponding pixel coordinate for the given local
+            // direction
+            auto local_dir = m_transform->inverse(direction);
+            if (m_transform)
+            {
+                auto x = local_dir.x();
+                auto y = local_dir.y();
+                auto z = local_dir.z();
+                float phi, theta;
+                theta = atan2(sqrt(std::pow(x, 2) + std::pow(z, 2)), y);
+                phi = atan2(z, x);
+                phi -= Pi;
 
-        // implement better importance sampling here, if you ever need it
-        // (useful for environment maps with bright tiny light sources, like the
-        // sun for example)
+                warped.x() = theta / Pi;
+                warped.y() = (phi + Pi) / (2 * Pi);
+            }
+            return {
+                .value = m_texture->evaluate(warped),
+            };
+        }
 
-        return {
-            .wi     = direction,
-            .weight = E.value / Inv4Pi,
-            .distance = Infinity,
-        };
-    }
+        DirectLightSample sampleDirect(const Point &origin,
+                                       Sampler &rng) const override
+        {
+            Vector direction = squareToUniformSphere(rng.next2D());
+            auto E = evaluate(direction);
 
-    std::string toString() const override {
-        return tfm::format("EnvironmentMap[\n"
-                           "  texture = %s,\n"
-                           "  transform = %s\n"
-                           "]",
-                           indent(m_texture), indent(m_transform));
-    }
-};
+            // implement better importance sampling here, if you ever need it
+            // (useful for environment maps with bright tiny light sources, like the
+            // sun for example)
+
+            return {
+                .wi = direction,
+                .weight = E.value / Inv4Pi,
+                .distance = Infinity,
+            };
+        }
+
+        std::string toString() const override
+        {
+            return tfm::format("EnvironmentMap[\n"
+                               "  texture = %s,\n"
+                               "  transform = %s\n"
+                               "]",
+                               indent(m_texture), indent(m_transform));
+        }
+    };
 
 } // namespace lightwave
 
