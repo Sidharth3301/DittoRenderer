c5cfa2bfa83ff12258acf949ea346b4bd9013a6b
Sidharth Nair <iamsidharthnair@gmail.com>
2023-12-16 17:59:24 +0100

    roughconductor passing

--- src/bsdfs/roughconductor.cpp
+++ src/bsdfs/roughconductor.cpp
@@ -2,50 +2,64 @@
 #include "microfacet.hpp"
 #include <lightwave.hpp>
 
-namespace lightwave {
+namespace lightwave
+{
 
-class RoughConductor : public Bsdf {
-    ref<Texture> m_reflectance;
-    ref<Texture> m_roughness;
+    class RoughConductor : public Bsdf
+    {
+        ref<Texture> m_reflectance;
+        ref<Texture> m_roughness;
 
-public:
-    RoughConductor(const Properties &properties) {
-        m_reflectance = properties.get<Texture>("reflectance");
-        m_roughness   = properties.get<Texture>("roughness");
-    }
+    public:
+        RoughConductor(const Properties &properties)
+        {
+            m_reflectance = properties.get<Texture>("reflectance");
+            m_roughness = properties.get<Texture>("roughness");
+        }
 
-    BsdfEval evaluate(const Point2 &uv, const Vector &wo,
-                      const Vector &wi) const override {
-        // Using the squared roughness parameter results in a more gradual
-        // transition from specular to rough. For numerical stability, we avoid
-        // extremely specular distributions (alpha values below 10^-3)
-        const auto alpha = std::max(float(1e-3), sqr(m_roughness->scalar(uv)));
+        BsdfEval evaluate(const Point2 &uv, const Vector &wo,
+                          const Vector &wi) const override
+        {
+            // Using the squared roughness parameter results in a more gradual
+            // transition from specular to rough. For numerical stability, we avoid
+            // extremely specular distributions (alpha values below 10^-3)
+            const auto alpha = std::max(float(1e-3), sqr(m_roughness->scalar(uv)));
 
-        NOT_IMPLEMENTED
+            auto mfacet_normal = (wi + wo).normalized();
+            auto reflectance = m_reflectance->evaluate(uv);
+            auto D = microfacet::evaluateGGX(alpha, mfacet_normal);
+            auto Gwi = microfacet::smithG1(alpha, mfacet_normal, wi);
+            auto Gwo = microfacet::smithG1(alpha, mfacet_normal, wo);
+            auto fr = reflectance * D * Gwi * Gwo / (4 * fabs(Frame::cosTheta(wi) * Frame::cosTheta(wo)));
+            return BsdfEval{.value = fr};
+            // hints:
+            // * the microfacet normal can be computed from `wi' and `wo'
+        }
 
-        // hints:
-        // * the microfacet normal can be computed from `wi' and `wo'
-    }
+        BsdfSample sample(const Point2 &uv, const Vector &wo,
+                          Sampler &rng) const override
+        {
+            const auto alpha = std::max(float(1e-3), sqr(m_roughness->scalar(uv)));
+            BsdfSample samp;
+            auto mf_normal = microfacet::sampleGGXVNDF(alpha, wo, rng.next2D());
+            samp.wi = reflect(wo, mf_normal);
+            auto sign = Frame::cosTheta(samp.wi) > 0 ? 1 : -1;
+            samp.weight = m_reflectance->evaluate(uv) * microfacet::smithG1(alpha, mf_normal, samp.wi) * sign;
+            return samp;
+            // hints:
+            // * do not forget to cancel out as many terms from your equations as possible!
+            //   (the resulting sample weight is only a product of two factors)
+        }
 
-    BsdfSample sample(const Point2 &uv, const Vector &wo,
-                      Sampler &rng) const override {
-        const auto alpha = std::max(float(1e-3), sqr(m_roughness->scalar(uv)));
-
-        NOT_IMPLEMENTED
-        
-        // hints:
-        // * do not forget to cancel out as many terms from your equations as possible!
-        //   (the resulting sample weight is only a product of two factors)
-    }
-
-    std::string toString() const override {
-        return tfm::format("RoughConductor[\n"
-                           "  reflectance = %s,\n"
-                           "  roughness = %s\n"
-                           "]",
-                           indent(m_reflectance), indent(m_roughness));
-    }
-};
+        std::string toString() const override
+        {
+            return tfm::format("RoughConductor[\n"
+                               "  reflectance = %s,\n"
+                               "  roughness = %s\n"
+                               "]",
+                               indent(m_reflectance), indent(m_roughness));
+        }
+    };
 
 } // namespace lightwave
 
