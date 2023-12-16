58c108e424ca19df74edd432425053c2cd39c621
Sidharth Nair <iamsidharthnair@gmail.com>
2023-12-16 15:03:05 +0100

    3.2 passing

--- src/bsdfs/conductor.cpp
+++ src/bsdfs/conductor.cpp
@@ -15,8 +15,8 @@ public:
         // the probability of a light sample picking exactly the direction `wi'
         // that results from reflecting `wo' is zero, hence we can just ignore
         // that case and always return black
-        if(isReflection(wo, wi))
-            return BsdfEval{.value = m_reflectance->evaluate(uv)};
+        // if(isReflection(wo, wi))
+        //     return BsdfEval{.value = m_reflectance->evaluate(uv)};
         return BsdfEval::invalid();
     }
 
@@ -24,16 +24,16 @@ public:
                       Sampler &rng) const override {
         BsdfSample b;
         b.wi = reflect(wo,Vector(0,0,1)).normalized(); // Reflect the incoming vector
-        b.weight = m_reflectance->evaluate(uv) * Frame::cosTheta(b.wi);
+        b.weight = m_reflectance->evaluate(uv);
         return b;
     }
-    bool isReflection(const Vector &wo, const Vector &wi) const {
-    // Assuming 'Frame' is a utility class that provides surface normal information
-        // Vector no = - wi + 2.f * wi.dot(Vector(0,0,1)) * Vector(0,0,1);
-        Vector normal = Vector(0,0,1); // Or another way to get the normal
-        Vector perfectReflection = reflect(wo, normal);
-        return (wi - perfectReflection).length() < 1e-8; // EPSILON is a small threshold value
-    }
+    // bool isReflection(const Vector &wo, const Vector &wi) const {
+    // // Assuming 'Frame' is a utility class that provides surface normal information
+    //     // Vector no = - wi + 2.f * wi.dot(Vector(0,0,1)) * Vector(0,0,1);
+    //     Vector normal = Vector(0,0,1); // Or another way to get the normal
+    //     Vector perfectReflection = reflect(wo, normal);
+    //     return (wi - perfectReflection).length() < 1e-8; // EPSILON is a small threshold value
+    // }
 
     std::string toString() const override {
         return tfm::format("Conductor[\n"
