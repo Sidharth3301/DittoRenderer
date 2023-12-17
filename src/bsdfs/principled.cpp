67f2d03fb71001d40dc2e388d8d205ce1c591770
Sidharth Nair <iamsidharthnair@gmail.com>
2023-12-18 00:08:27 +0100

    fixed broken branch

--- src/bsdfs/principled.cpp
+++ src/bsdfs/principled.cpp
@@ -108,20 +108,14 @@ namespace lightwave
                 .color = F * Color(1) + (1 - F) * metallic * baseColor,
             };
 
-        const auto diffuseAlbedo = diffuseLobe.color.mean();
-        const auto totalAlbedo =
-            diffuseLobe.color.mean() + metallicLobe.color.mean();
-        const auto diffuseAlbedo = diffuseLobe.color.mean();
-        const auto totalAlbedo =
-            diffuseLobe.color.mean() + metallicLobe.color.mean();
-        return {
-            .diffuseSelectionProb =
-                totalAlbedo > 0 ? diffuseAlbedo / totalAlbedo : 1.0f,
-                totalAlbedo > 0 ? diffuseAlbedo / totalAlbedo : 1.0f,
-            .diffuse  = diffuseLobe,
-            .metallic = metallicLobe,
-        };
-    }
+            return {
+                .diffuseSelectionProb =
+                    diffuseLobe.color.mean() /
+                    (diffuseLobe.color.mean() + metallicLobe.color.mean()),
+                .diffuse = diffuseLobe,
+                .metallic = metallicLobe,
+            };
+        }
 
     public:
         Principled(const Properties &properties)
