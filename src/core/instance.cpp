2f7152f7eead7148e1a58b44abced9b081f1cec1
sidharth <sina00002@stud.uni-saarland.de>
2024-02-03 22:59:30 +0100

    working area light

--- src/core/instance.cpp
+++ src/core/instance.cpp
@@ -3,112 +3,153 @@
 #include <lightwave/registry.hpp>
 #include <lightwave/sampler.hpp>
 
-namespace lightwave {
+namespace lightwave
+{
 
-    Vector Instance::applyNormal(SurfaceEvent &surf) const {
-    if (!m_transform) {
-        // fast path, if no transform is needed
-        return surf.frame.normal;
+    Vector Instance::applyNormal(SurfaceEvent &surf) const
+    {
+        if (!m_transform)
+        {
+            // fast path, if no transform is needed
+            return surf.frame.normal;
+        }
+        // hint: transform the normal here
+        Vector newNormal = m_transform->inverse(surf.frame.normal);
+        return newNormal;
     }
-    // hint: transform the normal here
-    Vector newNormal = m_transform->inverse(surf.frame.normal);
-    return newNormal;
-}
 
-void Instance::transformFrame(SurfaceEvent &surf) const {
-    // hints:
-    // * transform the hitpoint and frame here
-    // * if m_flipNormal is true, flip the direction of the bitangent (which in effect flips the normal)
-    // * make sure that the frame is orthonormal (you are free to change the bitangent for this, but keep
-    //   the direction of the transformed tangent the same)
+    void Instance::transformFrame(SurfaceEvent &surf) const
+    {
+        // hints:
+        // * transform the hitpoint and frame here
+        // * if m_flipNormal is true, flip the direction of the bitangent (which in effect flips the normal)
+        // * make sure that the frame is orthonormal (you are free to change the bitangent for this, but keep
+        //   the direction of the transformed tangent the same)
         surf.position = m_transform->apply(surf.position);
-        surf.frame = Frame(applyNormal(surf)); 
-        surf.pdf *= surf.frame.bitangent.cross(surf.frame.tangent).length();
-}
+        if (m_normal)
+        {
+            surf.frame = Frame(applyNormal(surf));
+        }
+        else
+        {
+            surf.frame.tangent = m_transform->apply(surf.frame.tangent).normalized();
+            Vector bitangent = m_transform->apply(surf.frame.bitangent);
+            float proj = surf.frame.tangent.dot(bitangent);
 
-bool Instance::intersect(const Ray &worldRay, Intersection &its, Sampler &rng) const {
-    if (!m_transform) {
-        // fast path, if no transform is needed
-        Ray localRay = worldRay;
-        if (m_shape->intersect(localRay, its, rng)) {
+            surf.frame.bitangent = (bitangent - proj * surf.frame.tangent).normalized();
+            if (m_flipNormal)
+            {
+                surf.frame.bitangent = -surf.frame.bitangent;
+            }
+
+            surf.frame.normal = surf.frame.tangent.cross(surf.frame.bitangent).normalized();
+        }
+        surf.pdf *= surf.frame.bitangent.cross(surf.frame.tangent).length();
+    }
+
+    bool Instance::intersect(const Ray &worldRay, Intersection &its, Sampler &rng) const
+    {
+        if (!m_transform)
+        {
+            // fast path, if no transform is needed
+            Ray localRay = worldRay;
+            if (m_shape->intersect(localRay, its, rng))
+            {
+                its.instance = this;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        const float previousT = its.t;
+        Ray localRay;
+
+        // hints:
+        // * transform the ray (do not forget to normalize!)
+        // * how does its.t need to change?
+        // m_transform -> apply contains object - world
+        localRay = m_transform->inverse(worldRay);
+        float scaleNum = localRay.direction.length();
+        localRay.direction = localRay.direction.normalized();
+        its.t = its.t * scaleNum; // its.t now contains the t in object space
+
+        const bool wasIntersected = m_shape->intersect(localRay, its, rng);
+
+        if (wasIntersected)
+        {
+            // hint: how does its.t need to change?
+            if (m_normal)
+            {
+                auto normal_texture = m_normal->evaluate(its.uv);
+
+                auto normal_rgb = Vector(normal_texture.r(), normal_texture.g(), normal_texture.b());
+                normal_rgb = normal_rgb * 2 - Vector(1, 1, 1);
+                normal_rgb = normal_rgb.x() * its.frame.tangent + normal_rgb.y() * its.frame.bitangent + 5 * normal_rgb.z() * its.frame.normal;
+                its.frame.normal = normal_rgb;
+            }
+
             its.instance = this;
+            transformFrame(its);
+            its.t = its.t / scaleNum;
             return true;
-        } else {
+        }
+        else
+        {
+            its.t = previousT;
             return false;
         }
     }
-    const float previousT = its.t;
-    Ray localRay;
-    
 
-    // hints:
-    // * transform the ray (do not forget to normalize!)
-    // * how does its.t need to change?
-    //m_transform -> apply contains object - world
-    localRay = m_transform->inverse(worldRay);
-    float scaleNum = localRay.direction.length();
-    localRay.direction = localRay.direction.normalized();
-    its.t = its.t * scaleNum;  // its.t now contains the t in object space
-    
-    const bool wasIntersected = m_shape->intersect(localRay, its, rng);
-    
-
-    if (wasIntersected) {
-        // hint: how does its.t need to change?
-        auto normal_texture = m_normal->evaluate(its.uv);
-        auto normal_rgb = Vector(normal_texture.r(), normal_texture.g(), normal_texture.b());
-        normal_rgb = normal_rgb * 2 - Vector(1, 1, 1);
-        normal_rgb = normal_rgb.x() * its.frame.tangent + normal_rgb.y() * its.frame.bitangent + 5 * normal_rgb.z() * its.frame.normal;
-        its.frame.normal = normal_rgb;
-        its.instance = this;
-        transformFrame(its);
-        its.t = its.t / scaleNum;
-        return true;
-    } else {
-        its.t = previousT;
-        return false;
-    }
-}
-
-Bounds Instance::getBoundingBox() const {
-    if (!m_transform) {
-        // fast path
-        return m_shape->getBoundingBox();
-    }
-
-    const Bounds untransformedAABB = m_shape->getBoundingBox();
-    if (untransformedAABB.isUnbounded()) {
-        return Bounds::full();
-    }
-
-    Bounds result;
-    for (int point = 0; point < 8; point++) {
-        Point p = untransformedAABB.min();
-        for (int dim = 0; dim < p.Dimension; dim++) {
-            if ((point >> dim) & 1) {
-                p[dim] = untransformedAABB.max()[dim];
-            }
+    Bounds Instance::getBoundingBox() const
+    {
+        if (!m_transform)
+        {
+            // fast path
+            return m_shape->getBoundingBox();
         }
-        p = m_transform->apply(p);
-        result.extend(p);
+
+        const Bounds untransformedAABB = m_shape->getBoundingBox();
+        if (untransformedAABB.isUnbounded())
+        {
+            return Bounds::full();
+        }
+
+        Bounds result;
+        for (int point = 0; point < 8; point++)
+        {
+            Point p = untransformedAABB.min();
+            for (int dim = 0; dim < p.Dimension; dim++)
+            {
+                if ((point >> dim) & 1)
+                {
+                    p[dim] = untransformedAABB.max()[dim];
+                }
+            }
+            p = m_transform->apply(p);
+            result.extend(p);
+        }
+        return result;
     }
-    return result;
-}
 
-Point Instance::getCentroid() const {
-    if (!m_transform) {
-        // fast path
-        return m_shape->getCentroid();
+    Point Instance::getCentroid() const
+    {
+        if (!m_transform)
+        {
+            // fast path
+            return m_shape->getCentroid();
+        }
+
+        return m_transform->apply(m_shape->getCentroid());
     }
 
-    return m_transform->apply(m_shape->getCentroid());
-}
-
-AreaSample Instance::sampleArea(Sampler &rng) const {
-    AreaSample sample = m_shape->sampleArea(rng);
-    transformFrame(sample);
-    return sample;
-}
+    AreaSample Instance::sampleArea(Sampler &rng) const
+    {
+        AreaSample sample = m_shape->sampleArea(rng);
+        transformFrame(sample);
+        return sample;
+    }
 
 }
 
